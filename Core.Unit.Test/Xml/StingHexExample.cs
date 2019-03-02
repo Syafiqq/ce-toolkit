00354aef2743d2258abf4ec09cf23e2e84508cd9
Syafiqq <syafiq.rezpector@gmail.com>
2019-03-02 07:25:29 +0700

    Add minus checking

--- Core.Unit.Test/Xml/StingHexExample.cs
+++ Core.Unit.Test/Xml/StingHexExample.cs
@@ -41,5 +41,45 @@ namespace Core.Unit.Test.Xml
             Assert.AreEqual(a, y1);
             Assert.AreEqual(8, y.Length);
         }
+        
+        [Test]
+        public void TestDecodeMinusHex()
+        {
+            var x = "-0000000F";
+            var s = '+';
+            if (x[0] == '-' || x[0] == '+')
+            {
+                s = x[0];
+                x = x.Remove(0, 1);
+            }
+
+            var y = Hex.Decode($"{"".PadRight(16 - x.Length, '0')}{x}");
+            var a = new byte[] {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xF};
+
+            Assert.AreEqual(a, y);
+            Assert.AreEqual(a.Length, y.Length);
+        }
+        
+        [Test]
+        public void TestMinusHexToLong()
+        {
+            var x = "-F";
+            var s = '+';
+            if (x[0] == '-' || x[0] == '+')
+            {
+                s = x[0];
+                x = x.Remove(0, 1);
+            }
+            var y = Hex.Decode($"{"".PadRight(16 - x.Length, '0')}{x}");
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(y);
+            var y1 = BitConverter.ToInt64(y, 0);
+            if (s == '-')
+                y1 = 0 - y1;
+            var a = -15L;
+            
+            Assert.AreEqual(a, y1);
+            Assert.AreEqual(8, y.Length);
+        }
     }
 }
