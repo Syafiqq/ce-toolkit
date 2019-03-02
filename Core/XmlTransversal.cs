f4111039ac7102225603bbe4f105a2759470afeb
Syafiqq <syafiq.rezpector@gmail.com>
2019-03-02 07:45:27 +0700

    Fixing xml modifier

--- Core/XmlTransversal.cs
+++ Core/XmlTransversal.cs
@@ -3,55 +3,9 @@ using System.Xml;
 
 namespace Core
 {
-    public class XmlTransversal
+    public static class XmlTransversal
     {
-        public long Modifier { get; private set; }
-
-        private XmlTransversal()
-        {
-        }
-
-        public static XmlTransversal WithConfig(long modifier)
-        {
-            var instance = new XmlTransversal();
-            instance.Modifier = modifier;
-            return instance;
-        }
-
-        public void Filter(XmlElement el)
-        {
-            if (el?.Name == null)
-                return;
-
-            switch (el.Name.ToLowerInvariant())
-            {
-                case "laststate":
-                    ProcLastState(el);
-                    break;
-                case "address":
-                    ProcAddress(el);
-                    break;
-            }
-        }
-
-        private void ProcAddress(XmlElement el)
-        {
-            var addr = Convert.ToInt64(el.InnerText, 16);
-            if (addr != 0L)
-                el.InnerText = Convert.ToString(addr + Modifier, 16).ToUpperInvariant();
-        }
-
-        private void ProcLastState(XmlElement el)
-        {
-            if (el.HasAttribute("RealAddress"))
-            {
-                var addr = Convert.ToInt64(el.GetAttribute("RealAddress"), 16);
-                if (addr != 0L)
-                    el.SetAttribute("RealAddress", Convert.ToString(addr + Modifier, 16).ToUpperInvariant());
-            }
-        }
-
-        public void Transverse(XmlNode node, Action<XmlElement> action)
+        public static void Transverse(XmlNode node, Action<XmlElement> action)
         {
             if (node == null)
                 return;
