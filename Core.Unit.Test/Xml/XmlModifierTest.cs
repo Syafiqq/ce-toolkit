cecf7986ffdeaafc33e42e3809f9e4e1c6d3d9c7
Syafiqq <syafiq.rezpector@gmail.com>
2019-03-02 07:46:28 +0700

    Fix testing

--- Core.Unit.Test/Xml/XmlModifierTest.cs
+++ Core.Unit.Test/Xml/XmlModifierTest.cs
@@ -22,58 +22,8 @@ namespace Core.Unit.Test.Xml
             var doc = new XmlDocument();
             doc.Load(file);
             XmlNode root = doc.DocumentElement;
-            Transverse(root, Filter);
+            XmlTransversal.Transverse(root, XmlModifier.WithConfig(0).Filter);
             doc.Save(file);
-        }
-
-        private void Filter(XmlElement el)
-        {
-            if (el?.Name == null)
-                return;
-
-            switch (el.Name.ToLowerInvariant())
-            {
-                case "laststate":
-                    ProcLastState(el);
-                    break;
-                case "address":
-                    ProcAddress(el);
-                    break;
-            }
-        }
-
-        private void ProcAddress(XmlElement el)
-        {
-            var addr = Convert.ToInt64(el.InnerText, 16);
-            if(addr != 0L)
-                el.InnerText = Convert.ToString(addr + 1, 16).ToUpperInvariant();
-        }
-
-        private void ProcLastState(XmlElement el)
-        {
-            if (el.HasAttribute("RealAddress"))
-            {
-                var addr = Convert.ToInt64(el.GetAttribute("RealAddress"), 16);
-                if(addr != 0L)
-                    el.SetAttribute("RealAddress", Convert.ToString(addr + 1, 16).ToUpperInvariant());
-            }
-        }
-
-        void Transverse(XmlNode node, Action<XmlElement> action)
-        {
-            if (node == null)
-                return;
-            if (node is XmlElement n1)
-                action(n1);
-
-            if (node.HasChildNodes)
-            {
-                foreach (XmlNode n in node.ChildNodes)
-                {
-                    if (n is XmlElement n2)
-                        Transverse(n2, action);
-                }
-            }
         }
     }
 }
